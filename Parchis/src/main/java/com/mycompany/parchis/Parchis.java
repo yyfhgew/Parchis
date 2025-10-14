fd74035615627d94f16dca8231ffa12f181f0d74
johanna Villalobos <zjohana185 gmail.com>
2025-10-14 08:06:01 -0600

    Agregar proyecto Parchis al repositorio

---
+++ Parchis/src/main/java/com/mycompany/parchis/Parchis.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.parchis;
+
+/**
+ *
+ * @author gx160
+ */
+public class Parchis {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
